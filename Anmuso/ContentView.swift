afd2f626d28b2f058d7043fa285eabd0d65106e5
Kamikuz <yxue@uvm.edu>
2020-07-12 23:42:29 -0400

    Initial Commit

---
+++ Anmuso/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Anmuso
+//
+//  Created by KAMIKU on 7/12/20.
+//  Copyright © 2020 Anmuso. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
